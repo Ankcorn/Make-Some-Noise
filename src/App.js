7b34c927ac9d1bdf15172fa4f171e4c139728329
ThomasAnkcorn <thomasankcorn@gmail.com>
2019-05-19 22:25:20 +0100

    ROFL

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import useVolume from './useVolume';
+import pic from './cat.png';
+
 function App() {
-  const [volume, setVolume] = useState(0)
+  const [size, setVolume] = useState(0)
+  const [cats, setCats] = useState([])
+  
+  function appendCat() {
+    setCats([...cats, {x: Math.floor(Math.random() * 100) + 1, y: Math.floor(Math.random() * 100) + 1}])
+  }
   useVolume((v) => {
-    setVolume(v)
+    if(v > 10) {
+      appendCat()
+    } else if(v===0 && cats.length > 1){
+      console.log(cats)
+      cats.shift()
+      setCats(cats)
+    }
+    setVolume(v * 4 + 100);
   })
+  
+  console.log(cats)
   return (
     <div className="App">
       <header className="App-header">
-        <h1>{volume}</h1>
+        <img src={pic} alt="a cat" width={size} />
+        <div style={{position: 'fixed', top: 0, width: '100vw', height: '100vh'}}>
+          {cats && cats.map(el => <img key={el.x+el.y} style={{position: 'fixed', top: `${el.y}vh`, left: `${el.x}vw`}} src={pic} alt="a cat" width={size} />)}
+        </div>
       </header>
     </div>
   );
