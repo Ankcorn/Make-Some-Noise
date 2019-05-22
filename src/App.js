de2fc5ea7d3b769d520077656f650408c6ab1ad9
ThomasAnkcorn <thomasankcorn@gmail.com>
2019-05-22 17:13:25 +0100

    moar kittys

--- src/App.js
+++ src/App.js
@@ -1,33 +1,52 @@
 import React, { useState } from 'react';
 import './App.css';
 import useVolume from './useVolume';
-import pic from './cat.png';
+import grumpy from './grumpy.png';
+import attack from './attack.png';
+import meow from './meow.png';
+import sad from './sad.png';
+
+import posed, { PoseGroup } from 'react-pose';
+
+
+const catpics = [grumpy, attack, meow, sad]
+const Img = posed.img({
+  enter: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      y: { type: 'spring', stiffness: 1000, damping: 15 },
+      default: { duration: 50 }
+    }
+  },
+  exit: {
+    y: 100,
+    delay: 100,
+    transition: { duration: 150 }
+  }
+});
 
 function App() {
-  const [size, setVolume] = useState(0)
   const [cats, setCats] = useState([])
   
-  function appendCat() {
-    setCats([...cats, {x: Math.floor(Math.random() * 100) + 1, y: Math.floor(Math.random() * 100) + 1}])
+  function appendCat(volume) {
+    setCats([...cats, {x: Math.floor(Math.random() * 100) + 1, y: volume * 2, volume: volume * 7 + 100}])
   }
   useVolume((v) => {
-    if(v > 10) {
-      appendCat()
-    } else if(v===0 && cats.length > 1){
-      console.log(cats)
-      cats.shift()
-      setCats(cats)
+    console.log(v)
+    if(v > 2) {
+      appendCat(v)
+    } else if(v === 10){
+      setCats([])
     }
-    setVolume(v * 4 + 100);
   })
   
   console.log(cats)
   return (
     <div className="App">
       <header className="App-header">
-        <img src={pic} alt="a cat" width={size} />
         <div style={{position: 'fixed', top: 0, width: '100vw', height: '100vh'}}>
-          {cats && cats.map(el => <img key={el.x+el.y} style={{position: 'fixed', top: `${el.y}vh`, left: `${el.x}vw`}} src={pic} alt="a cat" width={size} />)}
+          <PoseGroup>{cats && cats.map(el => <Img key={el.x+el.y} style={{position: 'fixed', bottom: `${el.y}vh `, left: `${el.x}vw`}} src={catpics[Math.floor(Math.random() * catpics.length)]} alt="a cat" width={el.volume} />)}</PoseGroup>
         </div>
       </header>
     </div>
